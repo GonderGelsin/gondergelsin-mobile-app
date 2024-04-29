10469b550205a84a633eb9a0620d3c2f8aa54a30
emregurs3s <emregursesanane@gmail.com>
2024-04-30 02:32:45 +0300

    Home Page bitti tek sağ alt tafa canlı destek butonu koyamadım onun için aramızda tartışırız bide main darta çıktıyı alabilmek için home screenin adresini ekledim

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/pages/home/home_screen.dart';
 import 'package:flutter_application_1/pages/splash/splash_screen.dart';
 // import 'package:flutter_application_1/pages/welcome_page.dart';
 
