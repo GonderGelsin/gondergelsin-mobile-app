91f8d9320492c36774ca9f83eb306342d17b0424
Umut Topalak <umuttopalak@hotmail.com>
2024-05-15 21:21:39 +0300

    wrong login page import & route removed

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/forgot_password/forgot_password_screen.dart';
+import 'package:flutter_application_1/pages/home/components/body.dart';
 import 'package:flutter_application_1/pages/home/home_screen.dart';
 import 'package:flutter_application_1/pages/login_succes/components/body.dart';
 import 'package:flutter_application_1/pages/login_succes/login_succes_screen.dart';
-import 'package:flutter_application_1/pages/home/components/body.dart';
 import 'package:flutter_application_1/pages/profile/profile_pages.dart';
 import 'package:flutter_application_1/pages/sign_in/sign_in_screen.dart';
 import 'package:flutter_application_1/pages/sign_up/sign_up_screen.dart';
@@ -11,7 +11,6 @@ import 'package:flutter_application_1/pages/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (context) => SplashScreen(),
-  LoginPage.routeName: (context) => LoginPage(),
   ProfileScreen.routeName: (context) =>  ProfileScreen(),
   SignInScreen.routeName: (context) => SignInScreen(),
   ForgotPasswordScreen.routeName: (context) => ForgotPasswordScreen(),
