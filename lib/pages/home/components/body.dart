f4712ca808422565c9685650238ec12c2010e190
Umut Topalak <umuttopalak@hotmail.com>
2024-06-01 04:04:54 +0300

    campaign added

--- lib/pages/home/components/body.dart
+++ lib/pages/home/components/body.dart
@@ -26,6 +26,18 @@ class _BodyState extends State<Body> {
   double _containerHeight = getProportionateScreenHeight(120);
   List<Order> orders = [];
 
+  List<String> imageList = [
+    "assets/images/vecteezy_gift-with-golden-ribbon.jpg",
+    "assets/images/vecteezy_isometric-style-deliver.jpg",
+    "assets/images/vecteezy_mobile-smart-phone-with.jpg",
+  ];
+
+  List<String> textTitle = [
+    "Detaylı Bilgi İçin Tıklayınız",
+    "Detaylı Bilgi İçin Tıklayınız",
+    "Detaylı Bilgi İçin Tıklayınız",
+  ];
+
   @override
   void initState() {
     super.initState();
@@ -75,6 +87,8 @@ class _BodyState extends State<Body> {
                     BuildOrdersContainer(),
                     SizedBox(height: getProportionateScreenHeight(20)),
                     BuildAnimatedContainers(),
+                    SizedBox(height: getProportionateScreenHeight(20)),
+                    BuildCampaignField(context),
                   ],
                 ),
               )
@@ -345,53 +359,84 @@ class _BodyState extends State<Body> {
     );
   }
 
-  Widget BuildOrderItem(Order order) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: Colors.grey[200],
-        borderRadius: BorderRadius.circular(10),
-      ),
+  Padding BuildCampaignField(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-            "Status: ${order.status}",
+            'Kampanyalar',
             style: TextStyle(
-              fontSize: 16,
+              fontSize: getProportionateScreenWidth(18),
               fontWeight: FontWeight.bold,
+              color: kPrimaryColor,
             ),
           ),
-          SizedBox(height: 5),
-          Text(
-            "Post Content: ${order.postContent}",
-            style: TextStyle(fontSize: 14),
-          ),
-          SizedBox(height: 5),
-          Text(
-            "Departure Address: ${order.departureAddress}",
-            style: TextStyle(fontSize: 14),
-          ),
-          SizedBox(height: 5),
-          Text(
-            "Arrival Address: ${order.arrivalAddress}",
-            style: TextStyle(fontSize: 14),
-          ),
-          SizedBox(height: 5),
-          Text(
-            "Vehicle Type: ${order.vehicleType}",
-            style: TextStyle(fontSize: 14),
-          ),
-          SizedBox(height: 5),
-          Text(
-            "Weight Limit: ${order.weightLimit}",
-            style: TextStyle(fontSize: 14),
-          ),
-          SizedBox(height: 5),
-          Text(
-            "Payment Method: ${order.paymentMethod}",
-            style: TextStyle(fontSize: 14),
+          SizedBox(height: getProportionateScreenHeight(10)),
+          Container(
+            height: getProportionateScreenHeight(200),
+            child: ListView.builder(
+              itemCount: imageList.length,
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: EdgeInsets.only(right: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        height: getProportionateScreenHeight(150),
+                        child: Stack(
+                          children: [
+                            InkWell(
+                              onTap: () {},
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: Image.asset(
+                                  width: getProportionateScreenWidth(200),
+                                  imageList[index],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: getProportionateScreenHeight(10),
+                      ),
+                      Container(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 10.0),
+                          child: Row(
+                            children: [
+                              Text(
+                                textTitle[index],
+                                style: TextStyle(
+                                  fontSize: getProportionateScreenWidth(13),
+                                  fontWeight: FontWeight.bold,
+                                  color: kSecondaryColor,
+                                ),
+                              ),
+                              SizedBox(
+                                width: getProportionateScreenWidth(10),
+                              ),
+                              Image.asset(
+                                'assets/icons/Arturo-Wibawa-Akar-Chevron-right-small.512.png',
+                                width: getProportionateScreenWidth(16),
+                                color: kSecondaryColor,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         ],
       ),
@@ -411,4 +456,71 @@ class _BodyState extends State<Body> {
       MaterialPageRoute(builder: (context) => ProfileScreen()),
     );
   }
+}
+
+Widget BuildOrderItem(Order order) {
+  return Container(
+    margin: EdgeInsets.symmetric(vertical: 10),
+    padding: EdgeInsets.all(10),
+    decoration: BoxDecoration(
+      color: Colors.grey[200],
+      borderRadius: BorderRadius.circular(10),
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Status: ${order.status}",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "Post Content: ${order.postContent}",
+          style: TextStyle(fontSize: 14),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "Departure Address: ${order.departureAddress}",
+          style: TextStyle(fontSize: 14),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "Arrival Address: ${order.arrivalAddress}",
+          style: TextStyle(fontSize: 14),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "Vehicle Type: ${order.vehicleType}",
+          style: TextStyle(fontSize: 14),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "Weight Limit: ${order.weightLimit}",
+          style: TextStyle(fontSize: 14),
+        ),
+        SizedBox(height: 5),
+        Text(
+          "Payment Method: ${order.paymentMethod}",
+          style: TextStyle(fontSize: 14),
+        ),
+      ],
+    ),
+  );
+}
+
+void _navigateToNotificationPage(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => NotificationPage()),
+  );
+}
+
+void _navigateToProfilePage(BuildContext context) {
+  Navigator.push(
+    context,
+    MaterialPageRoute(builder: (context) => ProfileScreen()),
+  );
 }
