cde481470985bca724e90a8ee708466b35dc8a81
Umut Topalak <umuttopalak@hotmail.com>
2024-06-01 03:48:47 +0300

    active orders

---
+++ lib/pages/home/components/service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter_application_1/pages/createOrder/components/order.dart';
+import 'package:flutter_application_1/services/authentication.dart'
+    as authentication;
+import 'package:http/http.dart' as http;
+
+Future<List<Order>> fetchOrders() async {
+  final authToken = await authentication.getStoredData('auth_token');
+  final response = await http.get(
+    Uri.parse('https://gondergelsin.pythonanywhere.com/order/active/'),
+    headers: {'Authorization': 'Token $authToken'},
+  );
+
+  if (response.statusCode == 200) {
+    final List<dynamic> responseData = jsonDecode(response.body);
+
+    List<Order> orders =
+        responseData.map((json) => Order.fromJson(json)).toList();
+    return orders;
+  } else {
+    throw Exception('Failed to load orders');
+  }
+}
